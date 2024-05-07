3234cfd12f4bb0bf985882127db4427668a5783b
nykhoa2405@gmail.com <nykhoa2405@gmail.com>
2024-05-07 15:18:25 +0700

    order detail

--- NguyenYKhoa0202/NguyenYKhoa0202/Controllers/CartController.cs
+++ NguyenYKhoa0202/NguyenYKhoa0202/Controllers/CartController.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Transactions;
+using TransactionScope = System.Transactions.TransactionScope;
 
 namespace NguyenYKhoa0202.Controllers
 {
     public class CartController : Controller
     {
+        NorthwindDataContext da = new NorthwindDataContext();
+
         // GET: Cart
         public List<CartModel> GetListCarts()
         {
@@ -47,6 +51,53 @@ namespace NguyenYKhoa0202.Controllers
 
             return RedirectToAction("ListCarts");
         }
+
+        public ActionResult OrderProduct()
+        { 
+            using(TransactionScope tranScope = new TransactionScope())
+            {
+                try
+                {
+                    Order order = new Order();
+                    order.OrderDate = DateTime.Now;
+                    da.Orders.InsertOnSubmit(order);
+                    da.SubmitChanges();
+
+                    List<CartModel> carts = GetListCarts();
+                    foreach(var item in carts)
+                    {
+                        Order_Detail d = new Order_Detail();
+                        d.OrderID = order.OrderID;
+                        d.ProductID = item.ProductID;
+                        d.Quantity = short.Parse(item.Quantity.ToString());
+                        d.UnitPrice = decimal.Parse(item.UnitPrice.ToString());
+                        d.Discount = 0;
+
+                        da.Order_Details.InsertOnSubmit(d);
+
+                    }
+                    da.SubmitChanges();
+                    tranScope.Complete();
+                    Session["CartModel"] = null;
+                }
+                catch (Exception)
+                {
+                    tranScope.Dispose();
+                    return RedirectToAction("ListCarts");
+                }
+            }
+
+
+            return RedirectToAction("OrderDetailsList");
+        }
+
+        public ActionResult OrderDetailsList()
+        {
+            var ds = da.Order_Details.OrderByDescending(s => s.OrderID).ToList();
+            return View(ds);
+        }
+
+
 
     }
 }
